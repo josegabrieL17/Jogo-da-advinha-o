ab4e338d0a0c88876c6dbaa238fbbd7da3bd8545
José <128156840+josegabrieL17@users.noreply.github.com>
2024-02-28 09:49:52 -0300

    Add files via upload

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -2,21 +2,24 @@ print('***********************************')
 print('*Bem vindo, ao JOGO DE ADIVINHAÇÃO*')
 print('***********************************')
 
+#definindo número secreto
 numeroSecreto = 38
 
-chuteString = input('Digite um número: ')
+#definindo o número de tentativas
+numeroTentativas = 3
 
+while(numeroTentativas > 0):
+    numeroTentativas = numeroTentativas -1
 
-print('Você digitou o número', chuteString)
+#recebendo o chute
+    chuteString = input('Digite um número: ')
+    print('Você digitou o número', chuteString)
+    chute = int(chuteString)
 
-chute = int(chuteString)
-
-if numeroSecreto == chute:
-    print('Você acertou!')
-elif(chute>numeroSecreto):
-    print("Você errou! o número é menor")
-else:
-    print('Você errou! O número secreto é maior')
-
-
-
+#declarando as condições
+    if numeroSecreto == chute:
+        print('Você acertou!')
+    elif(chute>numeroSecreto):
+        print("Você errou! o número é menor")
+    else:
+        print('Você errou! O número secreto é maior')
