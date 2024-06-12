4bf33a5b41178145657a12e7a4dd28f457cd86b8
José <128156840+josegabrieL17@users.noreply.github.com>
2024-06-12 08:58:13 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,7 +1,30 @@
 def jogar_forca():
 
-    print("**************************")
-    print("**Seja bem vindo a forca**")
-    print("**************************")
+    print("**********************************************************")
+    print("    BEM VINDO MEU NOBRE AO JOGO DA FORCA NESSA BAGAÇA")
+    print("**********************************************************")
 
-    print("fim de jogo")
+    
+    #Definir qual a palavra secreta
+    palavraSecreta = "GOJO"
+
+    enforcou = False
+    acertou = False
+
+
+    #Enquanto o jogador não se "enforcar" E não acertar a palavra, faça algo
+    while(not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+        index = 0
+        for letra in palavraSecreta:
+            if(chute.upper() == letra.upper):
+                print("VOCÊ ENCONTROU A LETRA {} NA POSIÇÃO {} MEU NOBRE".format(chute, index))
+            index = index + 1
+        print("escolha outra letra")
+       
+
+
+    print("END GAME")
+if(__name__ == "__main__"):
+    jogar_forca()
